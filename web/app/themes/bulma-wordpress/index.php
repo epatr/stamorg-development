f8c065e0a4a72efa51cb2f5dd00261ee468b2b10
Eric Patrick <erstpa@gmail.com>
2017-08-05 13:39:56 -0400

    Set up the views

--- web/app/themes/bulma-wordpress/index.php
+++ web/app/themes/bulma-wordpress/index.php
@@ -5,7 +5,9 @@ get_header();
 if (have_posts()) {
     while (have_posts()) {
         the_post();
+        echo '<ul>';
         get_template_part('templates/li', get_post_format());
+        echo '</ul>';
     }
 }
 
