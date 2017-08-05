0a0480d4fd6f3196d89724eb5e6a3c8e368511fb
Eric Patrick <erstpa@gmail.com>
2017-08-05 14:38:49 -0400

    Added singular view

---
+++ web/app/themes/bulma-wordpress/singular.php
@@ -0,0 +1,25 @@
+<?php
+
+get_header();
+
+?>
+
+<article>
+
+<?php
+if (have_posts()) {
+    while (have_posts()) {
+        the_post();
+?>
+
+        <?php get_template_part('templates/single', get_post_format()); ?>
+
+</article>
+        
+<?php
+    }
+}
+
+get_footer();
+
+?>
