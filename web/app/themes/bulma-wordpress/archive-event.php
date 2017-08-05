968a118a6275e703a9cde1f6bf394f2d5426f386
Eric Patrick <erstpa@gmail.com>
2017-08-05 19:12:15 -0400

    Set event orderby

--- web/app/themes/bulma-wordpress/archive-event.php
+++ web/app/themes/bulma-wordpress/archive-event.php
@@ -11,6 +11,8 @@ $todaysDate = date('Ymd');
 $eventQuery = array(
     'posts_per_page' => -1,
     'post_type' => 'event',
+	'order' => 'ASC',
+	'orderby' => 'event_date',
     'meta_key' => 'event_date',
     'meta_query' => array(
         array(
