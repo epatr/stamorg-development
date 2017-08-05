f5100c5e7f730da72b40a7c70746bee6388eb12f
Eric Patrick <erstpa@gmail.com>
2017-08-05 16:59:05 -0400

    Fixing the page view

--- web/app/themes/bulma-wordpress/templates/single.php
+++ web/app/themes/bulma-wordpress/templates/single.php
@@ -1,6 +1,8 @@
 <?php the_title( '<h1 class="title is-1">', '</h1>' ); ?>
 
-<div class="article__metadata">Posted <span><?php echo get_the_date(); ?></span> by <?php the_author_posts_link(); ?> <?php if(get_the_category()): echo ' in ' , the_category(); endif; ?></div>
+<?php if (!is_page()) : ?>
+    <div class="article__metadata">Posted <span><?php echo get_the_date(); ?></span> by <?php the_author_posts_link(); ?> <?php if(get_the_category()): echo ' in ' , the_category(); endif; ?></div>
+<?php endif; ?>
 <div class="article__content">
     <?php the_content(); ?>
 </div>
--- web/app/themes/bulma-wordpress/templates/summary.php
+++ web/app/themes/bulma-wordpress/templates/summary.php
@@ -1,4 +1,4 @@
-<a href="<?php the_permalink()"><h3 class="title is-3"><?php the_title(); ?></h3></a>
+<a href="<?php the_permalink(); ?>"><h3 class="title is-3"><?php the_title(); ?></h3></a>
 
 <div class="article__metadata">Posted <span><?php echo get_the_date(); ?></span> by <?php the_author_posts_link(); ?> <?php if(get_the_category()): echo ' in ' , the_category(); endif; ?></div>
 <div class="article__content">
