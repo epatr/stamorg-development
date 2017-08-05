915b39a14f848104b9fb7df78876b0b35bb92370
Eric Patrick <erstpa@gmail.com>
2017-08-05 12:45:28 -0400

    Added li template

--- web/app/themes/bulma-wordpress/index.php
+++ web/app/themes/bulma-wordpress/index.php
@@ -5,7 +5,7 @@ get_header();
 if (have_posts()) {
     while (have_posts()) {
         the_post();
-        get_template_part('templates/old-content', get_post_format());
+        get_template_part('templates/li', get_post_format());
     }
 }
 
---
+++ web/app/themes/bulma-wordpress/templates/li.php
@@ -0,0 +1,5 @@
+<?php
+
+the_title( '<li><a href="' . esc_url( get_permalink() ) . '">', '</a></li>' );
+
+?>
