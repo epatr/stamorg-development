b6eed7c5c0c6a41fb944af29d8ac959da5371b29
Eric Patrick <erstpa@gmail.com>
2017-08-05 19:10:49 -0400

    You are a pro

--- web/app/themes/bulma-wordpress/archive-event.php
+++ web/app/themes/bulma-wordpress/archive-event.php
@@ -26,7 +26,7 @@ $the_query = new WP_Query($eventQuery);
 
 if ($the_query->have_posts()) : 
     while ($the_query->have_posts()) :
-      $the_query(the_post());
+      $the_query->the_post();
 ?>
 
         <?php get_template_part('templates/li', get_post_format()); ?>
