8d2c878111af91850385fba65941f2e3d85af14d
Eric Patrick <erstpa@gmail.com>
2017-08-05 13:44:36 -0400

    This is a bad movie

--- web/app/themes/bulma-wordpress/index.php
+++ web/app/themes/bulma-wordpress/index.php
@@ -2,14 +2,19 @@
 
 get_header();
 
+?>
+
+<article>
+    <h1><? the_title(); ?></h1>
+
+<?php
 if (have_posts()) {
     while (have_posts()) {
         the_post();
 ?>
 
-<article>
-    <h1><? the_title(); ?></h1>
         <?php get_template_part('templates/li', get_post_format()); ?>
+
 </article>
         
 <?php
