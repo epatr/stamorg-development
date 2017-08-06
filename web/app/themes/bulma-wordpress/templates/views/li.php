c6c98a694ef8f9f3da5afdd7af4db4bdcc23dd08
Eric Patrick <erstpa@gmail.com>
2017-08-05 20:39:28 -0400

    Fixing my venue condition check

--- web/app/themes/bulma-wordpress/templates/views/li.php
+++ web/app/themes/bulma-wordpress/templates/views/li.php
@@ -1,15 +1,21 @@
 <li><?php
 
-if (get_post_type() !== 'venue') :
+// Checks to see which date, if any, it should print in the list view
+if (get_post_type() !== 'venue') : 
+
     if (get_post_type() === 'event') :
         $postDate = DateTime::createFromFormat('Ymd', $post->event_date)->format('l, F jS');
+
     else : 
         $postDate = get_the_date();
-    endif; // post = event or not event
-endif; // post != venue
 
+    endif;
 
     echo $postDate . ' - ';
+
+endif; // post != venue
+
+// Print out the title
 the_title( '<a href="' . esc_url( get_permalink() ) . '">', '</a>' );
 
 ?></li>
