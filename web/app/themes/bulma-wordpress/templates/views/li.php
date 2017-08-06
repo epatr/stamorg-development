28083bcd05a25eb1a76f575dd9a69e0fd1dfe8e8
Eric Patrick <erstpa@gmail.com>
2017-08-05 20:34:41 -0400

    Show event date if it's an event in the list view

--- web/app/themes/bulma-wordpress/templates/views/li.php
+++ web/app/themes/bulma-wordpress/templates/views/li.php
@@ -1,6 +1,12 @@
 <li><?php
 
-echo get_the_date() , ' - ';
+if (get_post_type() === 'event') :
+    $postDate = DateTime::createFromFormat('Ymd', $post->event_date)->format('l F jS');
+else : 
+    $postDate = get_the_date();
+endif;
+
+echo $postDate . ' - ';
 the_title( '<a href="' . esc_url( get_permalink() ) . '">', '</a>' );
 
 ?></li>
