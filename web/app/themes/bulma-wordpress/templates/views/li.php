dfcd8ce40edf1f2c98ca1ca82eea159110501baf
Eric Patrick <erstpa@gmail.com>
2017-08-05 20:36:34 -0400

    Don't show event date in venue list view

--- web/app/themes/bulma-wordpress/templates/views/li.php
+++ web/app/themes/bulma-wordpress/templates/views/li.php
@@ -1,12 +1,15 @@
 <li><?php
 
-if (get_post_type() === 'event') :
-    $postDate = DateTime::createFromFormat('Ymd', $post->event_date)->format('l F jS');
-else : 
-    $postDate = get_the_date();
-endif;
+if (get_post_type() !== 'venue') :
+    if (get_post_type() === 'event') :
+        $postDate = DateTime::createFromFormat('Ymd', $post->event_date)->format('l, F jS');
+    else : 
+        $postDate = get_the_date();
+    endif; // post = event or not event
+endif; // post != venue
 
-echo $postDate . ' - ';
+
+    echo $postDate . ' - ';
 the_title( '<a href="' . esc_url( get_permalink() ) . '">', '</a>' );
 
 ?></li>
