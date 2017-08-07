bab53105dfda5a31c897998c6f92ab679fe41f22
Eric Patrick <erstpa@gmail.com>
2017-08-07 13:51:42 -0400

    Google Maps per-google-haps

--- web/app/themes/bulma-wordpress/templates/snippets/venue-information.php
+++ web/app/themes/bulma-wordpress/templates/snippets/venue-information.php
@@ -1,3 +1,13 @@
+<div class="has-text-centered">
+    <iframe
+      width="600"
+      height="450"
+      frameborder="0" style="border:0"
+      src="https://www.google.com/maps/embed/v1/place?key=AIzaSyB0CBPoeI2IO_ep6mJ6lGrtAjuVS-3nHcQ
+        &q=<?= get_post_meta($eventVenueID, 'address', true); ?>" allowfullscreen>
+    </iframe>
+</div>
+
 <div class="section">
     <?php the_meta(); ?>
 </div>
