6567f845bdb24af7c1b06e3aaf08278d571190cc
Eric Patrick <erstpa@gmail.com>
2017-08-07 12:31:52 -0400

    Moving meta on event pages to a message box

--- web/app/themes/bulma-wordpress/templates/views/single.php
+++ web/app/themes/bulma-wordpress/templates/views/single.php
@@ -1,13 +1,27 @@
 <?php the_title( '<h1 class="title is-1">', '</h1>' ); ?>
 
-<?php if (!is_page() && !is_singular('event') && !is_singular('venue')) : ?>
+<?php if ( !is_page() && !is_singular('event') && !is_singular('venue') ) : ?>
     <div class="article__metadata">Posted <span><?php echo get_the_date(); ?></span> by <?php the_author_posts_link(); ?> <?php if(get_the_category()): echo ' in ' , the_category(); endif; ?></div>
 <?php endif; ?>
 <div class="article__content">
     <?php the_content(); ?>
 </div>
-<? if ( is_singular('venue') || is_singular('event')) : ?>
+
+<?php if ( is_singular('venue') ) : ?>
 <div class="section">
     <?php the_meta(); ?>
 </div>
 <?php endif; ?>
+
+<?php if ( is_singular('event') ) : ?>
+<div class="message is-dark">
+    <div class="message-header">Event Information</div>
+    <div class="message-body">
+        <ul>
+            <?php if (get_post_meta('event_date')) : $eventDate = DateTime::createFromFormat('Ymd', get_post_meta('event_date'))->format('l, F jS'); ?><li><?= $eventDate ?></li> <?php endif; ?>
+            <?php if (get_post_meta('website_url')) : ?><li><a href="<?php get_post_meta('website_url'); ?>" target="_blank"><?php get_post_meta('website_url'); ?></a></li> <?php endif; ?>
+        </ul>
+    </div>
+</div>
+
+<?php endif ?>
