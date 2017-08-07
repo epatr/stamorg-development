e0e3edc324eb8b0ed8900da42e09acc32d0c12cd
Eric Patrick <erstpa@gmail.com>
2017-08-07 17:06:22 -0400

    Use li-small temporarily for venue page

--- web/app/themes/bulma-wordpress/templates/views/li.php
+++ web/app/themes/bulma-wordpress/templates/views/li.php
@@ -1,19 +1,14 @@
 <li><?php
 
-// Checks to see which date, if any, it should print in the list view
-if (get_post_type() !== 'venue') : 
+// Checks to see which date it should print in the list view
 
-    if (get_post_type() === 'event') :
-        $postDate = DateTime::createFromFormat('Ymd', $post->event_date)->format('l, F jS');
+if (get_post_type() === 'event') :
+    $postDate = DateTime::createFromFormat('Ymd', $post->event_date)->format('l, F jS');
+else : 
+    $postDate = get_the_date();
+endif;
 
-    else : 
-        $postDate = get_the_date();
-
-    endif;
-
-    echo $postDate . ' - ';
-
-endif; // post != venue
+echo $postDate . ' - ';
 
 // Print out the title
 the_title( '<a href="' . esc_url( get_permalink() ) . '">', '</a>' );
