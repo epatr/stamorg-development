ec28afecbb0ba768ae1caeb57f3a0efafd9c6594
Eric Patrick <erstpa@gmail.com>
2017-08-07 13:46:58 -0400

    Moved event information into its own snippet

---
+++ web/app/themes/bulma-wordpress/templates/snippets/event-information.php
@@ -0,0 +1,19 @@
+
+<div class="message is-dark">
+    <div class="message-header">Event Information</div>
+    <div class="message-body">
+        <ul>
+            <li>When: <?php if ($post->event_date) : $eventDate = DateTime::createFromFormat('Ymd', $post->event_date)->format('l, F jS'); ?><?= $eventDate ?> <?php endif; ?> <? if (get_post_meta($post->ID, 'start_time', true)) : echo ' at ' . get_post_meta($post->ID, 'start_time', true); endif; ?></li>
+            <?php if (get_post_meta($post->ID, 'venue', true)) : $eventVenueID = $post->venue[0]; ?><li>Where: <a href="<?= get_the_permalink($eventVenueID); ?>" title="<?= get_post_meta($eventVenueID, 'address', true); ?>"><?= get_the_title($eventVenueID); ?></a></li><?php endif; ?>
+            <?php if (get_post_meta($post->ID, 'cost', true)) : ?><li>Cost: <?= get_post_meta($post->ID, 'cost', true); ?></li><?php endif; ?>
+            <?php if ($post->website_url) : ?><li><a href="<?php echo get_post_meta(get_the_ID(), 'website_url', true); ?>" target="_blank">Event Website</a></li> <?php endif; ?>
+        </ul>
+
+        <?php if (get_post_meta($post->ID, 'flyer')) : ?>
+        
+        <div class="has-text-centered"><?= wp_get_attachment_image($post->flyer, 'large'); ?></div>
+        
+        <?php endif; ?>
+
+    </div>
+</div>
--- web/app/themes/bulma-wordpress/templates/views/single.php
+++ web/app/themes/bulma-wordpress/templates/views/single.php
@@ -9,29 +9,9 @@
 </div>
 
 <?php if ( is_singular('venue') ) : ?>
-
 <?php get_template_part('templates/snippets/venue-information'); ?>
-
 <?php endif; ?>
 
 <?php if ( is_singular('event') ) : ?>
-<div class="message is-dark">
-    <div class="message-header">Event Information</div>
-    <div class="message-body">
-        <ul>
-            <li>When: <?php if ($post->event_date) : $eventDate = DateTime::createFromFormat('Ymd', $post->event_date)->format('l, F jS'); ?><?= $eventDate ?> <?php endif; ?> <? if (get_post_meta($post->ID, 'start_time', true)) : echo ' at ' . get_post_meta($post->ID, 'start_time', true); endif; ?></li>
-            <?php if (get_post_meta($post->ID, 'venue', true)) : $eventVenueID = $post->venue[0]; ?><li>Where: <a href="<?= get_the_permalink($eventVenueID); ?>" title="<?= get_post_meta($eventVenueID, 'address', true); ?>"><?= get_the_title($eventVenueID); ?></a></li><?php endif; ?>
-            <?php if (get_post_meta($post->ID, 'cost', true)) : ?><li>Cost: <?= get_post_meta($post->ID, 'cost', true); ?></li><?php endif; ?>
-            <?php if ($post->website_url) : ?><li><a href="<?php echo get_post_meta(get_the_ID(), 'website_url', true); ?>" target="_blank">Event Website</a></li> <?php endif; ?>
-        </ul>
-
-        <?php if (get_post_meta($post->ID, 'flyer')) : ?>
-        
-        <div class="has-text-centered"><?= wp_get_attachment_image($post->flyer, 'large'); ?></div>
-        
-        <?php endif; ?>
-
-    </div>
-</div>
-
+    <?php get_template_part('templates/snippets/event-information'); ?>
 <?php endif ?>
