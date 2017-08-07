88a6655f354233535712714809ffaa4e898e9f53
Eric Patrick <erstpa@gmail.com>
2017-08-07 16:11:17 -0400

    If you could only see the way

--- web/app/themes/bulma-wordpress/sidebar.php
+++ web/app/themes/bulma-wordpress/sidebar.php
@@ -4,13 +4,11 @@
     <div class="menu">
         <p class="menu-label">St. Augustine Music Project</p>
 
-        <div class="menu-wp_nav_menu">
             <?php wp_nav_menu(array(
                 'theme_location' => 'primary',
                 'menu_class' => 'menu-wp_nav_menu'
                 )); 
             ?>
-        </div>
     </div>
 
 </div>
