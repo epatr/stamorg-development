3d067b8ddf114bbd4be318d96a0bf1af54e372d4
Eric Patrick <erstpa@gmail.com>
2017-08-07 15:54:36 -0400

    Has addons

--- web/app/themes/bulma-wordpress/searchform.php
+++ web/app/themes/bulma-wordpress/searchform.php
@@ -1,14 +1,14 @@
 <?php $unique_id = esc_attr( uniqid( 'search-form-' ) ); ?>
 
 <form role="search" method="get" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-    <div class="field is-horizontal">
+    <div class="field is-horizontal has-addons">
 
         <div class="control has-icons-left">
             <input type="text" class="input is-medium search-field" id="<?= $unique_id; ?>" name="s">
             <span class="icon is-normal is-left"><i class="fa fa-search"></i></span>
         </div>
         <div class="control">
-            <button type="submit" class="button is-info">Search</button>
+            <button type="submit" class="button is-info is-medium">Search</button>
         </div>
 
     </div>
