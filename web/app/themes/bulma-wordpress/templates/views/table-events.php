f2d4bf83579038c48951ae6850a037b6f4e948a4
Eric Patrick <erstpa@gmail.com>
2017-08-07 16:44:43 -0400

    Come onnnnn

--- web/app/themes/bulma-wordpress/templates/views/table-events.php
+++ web/app/themes/bulma-wordpress/templates/views/table-events.php
@@ -1,6 +1,6 @@
 <?php
     $eventDate = DateTime::createFromFormat('Ymd', $post->event_date)->format('l, F jS');
-    $eventTitle = '<a href="' . get_the_permalink($post->ID) . '">' . the_title() . '</a>';
+    $eventTitle = '<a href="' . get_the_permalink($post->ID) . '">' . get_the_title() . '</a>';
     $eventVenueID = $post->venue[0];
     $eventVenue = '<a href="' . get_the_permalink($eventVenueID) . '" title="' . get_post_meta($eventVenueID, 'address', true) . '"> ' . get_the_title($eventVenueID) . '</a>';
     $eventCost = get_post_meta($post->ID, 'cost', true);
