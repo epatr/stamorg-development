21ee627183ac99d2fc73da35d251d0b81a317d69
Eric Patrick <erstpa@gmail.com>
2017-08-07 16:03:33 -0400

    Might this be all I need to do for menus to work

--- web/app/themes/bulma-wordpress/sidebar.php
+++ web/app/themes/bulma-wordpress/sidebar.php
@@ -3,8 +3,10 @@
 
     <p class="menu-label">St. Augustine Music Project</p>
 
-    <?php wp_nav_menu(array(
-        'theme_location' => 'primary',
-        )); 
-    ?>
+    <div class="menu-wp_nav_menu">
+        <?php wp_nav_menu(array(
+            'theme_location' => 'primary',
+            )); 
+        ?>
+    </div>
 </div>
