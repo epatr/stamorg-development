017eafce3727a852ee2d303df7c27a7e8722a74f
Eric Patrick <erstpa@gmail.com>
2017-08-07 16:41:36 -0400

    Why permalink no work

--- web/app/themes/bulma-wordpress/templates/views/table-events.php
+++ web/app/themes/bulma-wordpress/templates/views/table-events.php
@@ -1,6 +1,6 @@
 <?php
     $eventDate = DateTime::createFromFormat('Ymd', $post->event_date)->format('l, F jS');
-    $eventTitle = get_the_title( '<a href="' . esc_url( get_permalink() ) . '">', '</a>' );
+    $eventTitle = get_the_title( '<a href="' . esc_url( get_the_permalink($post->ID) ) . '">', '</a>' );
     $eventVenueID = $post->venue[0];
     $eventVenue = '<a href="' . get_the_permalink($eventVenueID) . ' title="' . get_post_meta($eventVenueID, 'address', true) . '"> ' . get_the_title($eventVenueID) . '</a>';
     $eventCost = get_post_meta($post->ID, 'cost', true);
