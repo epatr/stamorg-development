e05f5ef3aeeeaa05528f0a4acc511114e5ddd56a
Eric Patrick <erstpa@gmail.com>
2017-08-07 15:31:32 -0400

    Figures that didn't work

--- web/app/themes/bulma-wordpress/searchform.php
+++ web/app/themes/bulma-wordpress/searchform.php
@@ -1,8 +1,8 @@
 <?php $unique_id = esc_attr( uniqid( 'search-form-' ) ); ?>
 
-<p class="control has-icons-left">
-    <form role="search" method="get" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-    <input type="search" class="input is-medium search-field" id="<?= $unique_id; ?>" name="s">
-    <span class="icon is-normal is-left"><i class="fa fa-search"></i></span>
-    </form>
-</p>
+<form role="search" method="get" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+    <p class="control has-icons-left">
+        <input type="search" class="input is-medium search-field" id="<?= $unique_id; ?>" name="s">
+        <span class="icon is-normal is-left"><i class="fa fa-search"></i></span>
+    </p>
+</form>
