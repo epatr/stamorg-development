f7d16afdf5b1676f67088b116d5d94ea002821c5
Eric Patrick <erstpa@gmail.com>
2017-08-07 13:45:51 -0400

    Moved venue information into its own template

--- web/app/themes/bulma-wordpress/templates/views/single.php
+++ web/app/themes/bulma-wordpress/templates/views/single.php
@@ -9,32 +9,8 @@
 </div>
 
 <?php if ( is_singular('venue') ) : ?>
-<div class="section">
-    <?php the_meta(); ?>
-</div>
 
-<h3>Upcoming Events</h3>
-    <?php 
-
-    //$eventQuery = include(TEMPLATEPATH . '/' . 'templates/queries/events.php');
-    $eventQuery = bulmawordpress_event_query('events-by-venue', $post->ID);
-
-    $the_query = new WP_Query($eventQuery);
-
-    if ($the_query->have_posts()) : 
-        while ($the_query->have_posts()) :
-            $the_query->the_post();
-
-            get_template_part('templates/views/li', get_post_format());
-
-        endwhile;
-
-    endif;
-
-    wp_reset_query();
-
-    ?>
-
+<?php get_template_part('templates/snippets/venue-information'); ?>
 
 <?php endif; ?>
 
