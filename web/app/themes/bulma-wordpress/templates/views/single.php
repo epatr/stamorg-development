ed1473977ad093f5bb852a4501e5cf7b6c528667
Eric Patrick <erstpa@gmail.com>
2017-08-07 13:05:55 -0400

    Hyperlinking the venue

--- web/app/themes/bulma-wordpress/templates/views/single.php
+++ web/app/themes/bulma-wordpress/templates/views/single.php
@@ -21,7 +21,7 @@
         <ul>
             <li>Date: <?php if ($post->event_date) : $eventDate = DateTime::createFromFormat('Ymd', $post->event_date)->format('l, F jS'); ?><?= $eventDate ?><?php endif; if (get_post_meta($post->ID, 'start_time', true)) : echo get_post_meta($post->ID, 'start_time', true); endif; ?></li>
             <?php if ($post->website_url) : ?><li>More Information: <a href="<?php echo get_post_meta(get_the_ID(), 'website_url', true); ?>" target="_blank"><?php echo get_post_meta($post->ID, 'website_url', true); ?></a></li> <?php endif; ?>
-            <?php if (get_post_meta($post->ID, 'venue', true)) : ?><li>Location: <?= get_post_meta($post->ID, 'venue', true); ?></li><?php endif; ?>
+            <?php if (get_post_meta($post->ID, 'venue', true)) : $eventVenueID = $post->venue[0]; ?><li>Location: <a href="<?= get_the_permalink($eventVenueID); ?>"><?= get_the_title($eventVenueID); ?></a></li><?php endif; ?>
             <?php if (get_post_meta($post->ID, 'cost', true)) : ?><li>Cost: <?= get_post_meta($post->ID, 'cost', true); ?></li><?php endif; ?>
             <li>Meta: <?php 
                 $meta_values = get_post_meta(get_the_ID(), '', true);
