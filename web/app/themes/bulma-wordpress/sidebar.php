e57de05762902721a5ee1c0a58c116e1e5f795f3
Eric Patrick <erstpa@gmail.com>
2017-08-07 16:04:43 -0400

    Something is still off with this menu

--- web/app/themes/bulma-wordpress/sidebar.php
+++ web/app/themes/bulma-wordpress/sidebar.php
@@ -1,12 +1,15 @@
 
 <div class="content">
 
-    <p class="menu-label">St. Augustine Music Project</p>
-
-    <div class="menu-wp_nav_menu">
-        <?php wp_nav_menu(array(
-            'theme_location' => 'primary',
-            )); 
-        ?>
+    <div class="menu">
+        <p class="menu-label">St. Augustine Music Project</p>
+        
+        <div class="menu-wp_nav_menu">
+            <?php wp_nav_menu(array(
+                'theme_location' => 'primary',
+                )); 
+            ?>
+        </div>
     </div>
+
 </div>
