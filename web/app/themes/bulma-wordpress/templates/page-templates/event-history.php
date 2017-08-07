3f5b37dc7e114bd4f5b7dac2fa2cdfd1677786b5
Eric Patrick <erstpa@gmail.com>
2017-08-07 18:14:55 -0400

    Creating an archive of all shows

---
+++ web/app/themes/bulma-wordpress/templates/page-templates/event-history.php
@@ -0,0 +1,32 @@
+<?php
+
+/* Template Name: Event History */
+
+$eventQuery = bulmawordpress_event_query('events-all-desc');
+
+$the_query = new WP_Query($eventQuery);
+
+if ($the_query->have_posts()) : ?>
+<table class="table">
+    <thead>
+        <tr>
+            <th>Date</th>
+            <th>Bands</th>
+            <th>Venue</th>
+            <th>Cost</th>
+            <th>Flyer</th>
+        </tr>
+    </thead>
+<?php
+    while ($the_query->have_posts()) :
+        $the_query->the_post();
+
+        get_template_part('templates/views/table-events', get_post_format());
+
+    endwhile;
+?>
+</table>
+<?php
+endif;
+
+wp_reset_query();
---
+++ web/app/themes/bulma-wordpress/templates/queries/events-all-desc.php
@@ -0,0 +1,10 @@
+<?php
+
+// This file should have been called in a function with $variables as an integer
+return array(
+    'posts_per_page' => -1,
+    'post_type' => 'event',
+	'order' => 'DESC',
+	'orderby' => 'event_date'
+    )
+);
