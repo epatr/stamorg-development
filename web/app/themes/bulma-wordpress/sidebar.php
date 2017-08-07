583e002921a68477a441887848a5cbd5c3a33525
Eric Patrick <erstpa@gmail.com>
2017-08-07 15:20:53 -0400

    Trying to build a sidebar menu

--- web/app/themes/bulma-wordpress/sidebar.php
+++ web/app/themes/bulma-wordpress/sidebar.php
@@ -3,12 +3,8 @@
 
     <p class="menu-label">St. Augustine Music Project</p>
 
-    <ul class="menu-list">
     <?php wp_nav_menu(array(
-                    'theme_location' => 'primary',
-                    'link_before' => '<div class="menu-item">',
-                    'link_after' => '</div>'
-                )); ?>
-
-    </ul>
+        'theme_location' => 'primary',
+        )); 
+    ?>
 </div>
