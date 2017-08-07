2d1f432b7a212b61eb983fe7b5e5bcb777e0a2d0
Eric Patrick <erstpa@gmail.com>
2017-08-07 15:51:40 -0400

    I bet this search button doesn't work

--- web/app/themes/bulma-wordpress/searchform.php
+++ web/app/themes/bulma-wordpress/searchform.php
@@ -1,8 +1,11 @@
 <?php $unique_id = esc_attr( uniqid( 'search-form-' ) ); ?>
 
 <form role="search" method="get" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-    <p class="control has-icons-left">
+    <div class="control has-icons-left">
         <input type="search" class="input is-medium search-field" id="<?= $unique_id; ?>" name="s">
         <span class="icon is-normal is-left"><i class="fa fa-search"></i></span>
-    </p>
+    </div>
+    <div class="control">
+        <button type="submit" class="button is-info">Search</button>
+    </div>
 </form>
