ecca422ea243abceaf3d78027752e955da35b11a
Eric Patrick <erstpa@gmail.com>
2017-08-08 14:15:12 -0400

    Maybe featured images will work now

--- web/app/themes/bulma-wordpress/templates/views/summary.php
+++ web/app/themes/bulma-wordpress/templates/views/summary.php
@@ -1,5 +1,11 @@
 <article>
 
+<?php 
+if ( has_post_thumbnail() ) {
+	the_post_thumbnail('open-graph');
+} 
+?>
+
 <a href="<?php the_permalink(); ?>"><h3 class="title is-3"><?php the_title(); ?></h3></a>
 
 <div class="article__metadata">Posted <span><?php echo get_the_date(); ?></span> by <?php the_author_posts_link(); ?> <?php if(get_the_category()): echo ' in ' , the_category(); endif; ?></div>
